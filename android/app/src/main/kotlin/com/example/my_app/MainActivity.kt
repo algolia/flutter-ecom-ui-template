3b6eb99689f7aea6b4ec24b83998a7dbd1de65af
Mouaad Aallam <mouaad.aallam@algolia.com>
2022-02-03 16:56:42 +0100

    remove channels impl from android

--- android/app/src/main/kotlin/com/example/my_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/my_app/MainActivity.kt
@@ -6,19 +6,4 @@ import io.flutter.plugin.common.MethodChannel
 
 class MainActivity : FlutterActivity() {
 
-    private val algoliaAPIAdapter = AlgoliaAPIFlutterAdapter("latency", "1f6fd3a6fb973cb08419fe7d288fa4db")
-
-    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        super.configureFlutterEngine(flutterEngine)
-        MethodChannel(
-            flutterEngine.dartExecutor.binaryMessenger,
-            CHANNEL
-        ).setMethodCallHandler { call, result ->
-            algoliaAPIAdapter.perform(call, result)
-        }
-    }
-
-    companion object {
-        private const val CHANNEL = "com.algolia/api"
-    }
 }
